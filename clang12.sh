7e057814dbc7158bee188e058f15d8c1ccb326aa
Wahyu Hidayat <xxawhyuxx47@gmail.com>
2021-02-25 10:30:33 +0700

    clang12.sh: change builder
    Signed-off-by: Wahyu Hidayat <xxawhyuxx47@gmail.com>

--- clang12.sh
+++ clang12.sh
@@ -5,10 +5,10 @@ export ARCH=arm64
 
 make O=out ARCH=arm64 aegis_defconfig
 
-PATH="/home/wahyu/project/proton/bin:/home/wahyu/project/gcc/bin:${PATH}" \
-                            LD_LIBRARY_PATH="/home/wahyu/project/proton/lib:${LD_LIBRARY_PATH}" \
+PATH="/home/wahyu/proton/bin:/home/wahyu/gcc49/bin:${PATH}" \
+                            LD_LIBRARY_PATH="/home/wahyu/proton/lib:${LD_LIBRARY_PATH}" \
                             make -j$(nproc --all) O=out \
-                            CC=clang-12 \
+                            CC=clang-13 \
                             CROSS_COMPILE=aarch64-linux-android- \
                             CROSS_COMPILE_ARM32=arm-linux-androideabi- \
                             AR=llvm-ar \
